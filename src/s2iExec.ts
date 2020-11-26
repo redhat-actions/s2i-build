1be5b6fb24dc6f2685e028404666fac6a4e4572a
Tim Etchells <tetchell@redhat.com>
2020-11-26 18:04:13 -0500

    Package cleanup
    Signed-off-by: Tim Etchells <tetchell@redhat.com>

--- src/s2iExec.ts
+++ src/s2iExec.ts
@@ -18,24 +18,24 @@ export async function run(): Promise<void> {
     const pathContext = core.getInput('path_context');
     const logLevel = core.getInput('log_level');
     const runnerOS = process.env.RUNNER_OS;
-  
+
     const version = 'v1.3.1';
     core.debug(version);
     core.debug(runnerOS);
     core.debug(process.env.RUNNER_TEMP);
-    
+
     const binaryVersion: BinaryVersion = convertStringToBinaryVersion(version);
     const s2iBinary: FindBinaryStatus = await Installer.installS2i(binaryVersion, runnerOS);
 
     if (s2iBinary.found === false) {
       return Promise.reject(new Error(getReason(s2iBinary)));
     }
-  
+
     Installer.addS2iToPath(s2iBinary.path, runnerOS);
 
     const buildCmd = `build ${pathContext} ${builderImage} ${imageName}:${imageTag} --loglevel ${logLevel}`;
 
     await Command.execute(s2iBinary.path, buildCmd);
   }
-  
+
   run().catch(core.setFailed);
